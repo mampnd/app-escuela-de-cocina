1073957e9d59c7e4993a38aa8c626aa36032bb21
nustam <nustamichelle.97@gmail.com>
2020-05-24 16:15:37 -0500

    Ensayo para juntar paginas

--- js/my-app.js
+++ js/my-app.js
@@ -15,6 +15,10 @@ var app = new Framework7({
       path: '/about/',
       url: 'about.html',
     },
+       {
+      path: '/arroz/',
+      url: 'arco.html',
+    },
   ],
   // ... other parameters
 });
